e8d6bc24c695d9e0ad207bf65899a8b43c874195
Gabriel Fortin <gabfortin@icloud.com>
2023-12-04 21:35:08 +0000

    Day1

--- aoc2023/day1/part1.py
+++ aoc2023/day1/part1.py
@@ -1,11 +1,8 @@
 import re
 
-inputdata = list()
-with open("input.txt", "r") as f:
-    inputdata = f.readlines()
-
 def findCalibrationForLine(line):
     digits = re.findall("\d", line)
     return int(digits[0] + digits[-1])
-
-print(sum([findCalibrationForLine(line) for line in inputdata]))
+    
+with open("input.txt", "r") as f:
+	print(sum([findCalibrationForLine(line) for line in f.readlines()]))
