99e3f6f7c7707f10564ebacabf0f8b87cce6b01d
Gab Fortin <12024848+gabrielfortin@users.noreply.github.com>
2023-12-05 02:59:31 +0000

    Update day 3

--- aoc2023/day3/part2.py
+++ aoc2023/day3/part2.py
@@ -8,7 +8,6 @@ numbers = ["1","2","3","4","5","6","7","8","9","0"]
 
 gears = {}
 
-
 def build_num(i, j):
     number = schema[i][j]
     for x in range(1, 4):
@@ -22,7 +21,7 @@ def find_gears(i, j, number_len, number):
     x = -1
     while(x <= number_len):
         for y in [-1, 0, 1]:
-            if i+y < len(schema) and j+x < len(schema[i+y]) and schema[i+y][j+x] not in numbers and schema[i+y][j+x] not in [".", "\n"] and schema[i+y][j+x] == "*":
+            if i+y < len(schema) and j+x < len(schema[i+y]) and schema[i+y][j+x] == "*":
                 symbolindex = f"{i+y},{j+x}"
                 if symbolindex not in gears:
                     gears[symbolindex] = [int(number)]
