7cba7a5bedab4b8c92d8de8f29ea1a35db330a6d
Gab Fortin <12024848+gabrielfortin@users.noreply.github.com>
2023-12-05 02:55:18 +0000

    Update day 3

--- aoc2023/day3/part2.py
+++ aoc2023/day3/part2.py
@@ -6,7 +6,7 @@ with open("data.txt", "r") as f:
 
 numbers = ["1","2","3","4","5","6","7","8","9","0"]
 
-symbols = {}
+gears = {}
 
 
 def build_num(i, j):
@@ -18,7 +18,7 @@ def build_num(i, j):
             break
     return number
 
-def symbol_around_number(i, j, number_len, number):
+def find_gears(i, j, number_len, number):
     x = -1
     while(x <= number_len):
         for y in [-1, 0, 1]:
@@ -26,13 +26,12 @@ def symbol_around_number(i, j, number_len, number):
                 symbol = schema[i+y][j+x]
                 if symbol == "*":
                     symbolindex = f"{i+y},{j+x}"
-                    if symbolindex not in symbols:
-                        symbols[symbolindex] = [int(number)]
+                    if symbolindex not in gears:
+                        gears[symbolindex] = [int(number)]
                     else:
-                        symbols[symbolindex].append(int(number))
-                return True
+                        gears[symbolindex].append(int(number))
+                return
         x += 1
-    return False
 
 # Batir la table de gears
 for i in range(len(schema)):
@@ -40,13 +39,13 @@ for i in range(len(schema)):
     while j < len(schema[i]):
         if schema[i][j] in numbers:
             number = build_num(i, j)
-            symbol_around_number(i, j, len(number), number)
+            find_gears(i, j, len(number), number)
             j += len(number)
         j += 1
             
 # calculer les ratios
 sum = 0
-for k, v in symbols.items():
+for k, v in gears.items():
     if len(v) == 2:
         ratio = int(v[0]) * int(v[1])
         sum += ratio
