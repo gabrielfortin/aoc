b2732e0d8f54e16b943fcd25b17d6f36f99f5fff
Gab Fortin <12024848+gabrielfortin@users.noreply.github.com>
2023-12-08 05:17:26 +0000

    fix day8p1

--- aoc2023/day8/part1.py
+++ aoc2023/day8/part1.py
@@ -2,26 +2,15 @@ with open("input.txt", "r") as f:
     data = f.readlines()
 
 instruc = data[0].strip()
-print(instruc)
-
 
 lines = data[2:-1]
-lines += lines 
-lines += lines
-lines += lines
-lines += lines
-lines += lines
-lines += lines
 from pprint import pprint
 linesdict = {line[0:3]: {"L": line[7:10], "R": line[12:15]} for line in lines}
-pprint(linesdict)
 
 step = 0
 depassement = 0
 entry = "AAA"
-print(len(instruc))
 while(entry != "ZZZ"):
-    print(step)
     if len(instruc) == step:
         step = 0
         depassement += 1
