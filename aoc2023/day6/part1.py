c026a5dbf0b8b86c81e1d31b18b1f2ce83300b85
Gab Fortin <12024848+gabrielfortin@users.noreply.github.com>
2023-12-08 02:59:26 +0000

    fixed

--- aoc2023/day6/part1.py
+++ aoc2023/day6/part1.py
@@ -12,19 +12,14 @@ class Race:
         num_of_won = sum([((speed * (self.duration - speed)) > self.record) for speed in speeds])
         return num_of_won
         
-class Races:
-    def __init__(self, data):
-        lines = data.split("\n")
-        durations = re.findall("\d+", lines[0])
-        records = re.findall("\d+", lines[1])
-        self.races = [Race(durations[i], records[i]) for i in range(len(durations)) ]
-    
-    def part1(self):
-        result = 1
-        for race in self.races:
-            result *= race.simulate_all_races()
-        return result
+def part1():
+    lines = data.split("\n")
+    durations = re.findall("\d+", lines[0])
+    records = re.findall("\d+", lines[1])
+    result = 1
+    for race in [Race(durations[i], records[i]) for i in range(len(durations)) ]:
+        result *= race.simulate_all_races()
+    return result
 
-r = Races(data)
-print(r.part1())
+print(part1())
 
